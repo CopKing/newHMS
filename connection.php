9472b05b560b9946f0229a83179919b43b7c1220
Shahzaib Shaikh <shahzaib@gmail.com>
2025-08-16 17:54:53 +0500

    initial commit

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php
+ob_start();
+session_start();
+
+include 'functions.php';
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myhmsdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+?>
